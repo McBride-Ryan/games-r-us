a25eccb1bec6989bc39f2ba676efc8a79a425fe9
Ryan McBride <ryanmcbride@ryans-mbp.myfiosgateway.com>
2022-04-11 19:13:08 -0400

    Games-R-Us

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,9 +1,19 @@
 @extends('layouts.app')
 @section('content')
-body of world
-
-@foreach ($popularGames as $game)
-    <img src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="cover">
-@endforeach
+<div style="padding: 0 1%;"></div>
+    <div style="display: flex;
+    overflow-x: scroll;
+    overflow-y: hidden;
+    padding: 1.25rem;">
+        @foreach ($popularGames as $game)
+            <div >
+                    <img 
+                    
+                        class="row__poster"
+                        src="{{ Str::replaceFirst('thumb', 'cover_big', $game['cover']['url']) }}" alt="cover"
+                    >
+            </div>
+    @endforeach
+    </div>
     
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Games</title>
+    <style>
+        body,html{
+            margin: 0;
+        }
+        
+    </style>
 </head>
 <body>
     <div style="background-color: yellow">hello world</div>
